1a591ff915fa9dab61cb9a829b7d529894e5f56c
Miles <milesasp@gmail.com>
2019-12-14 12:29:27 +0800

    group route

--- src/main.go
+++ src/main.go
@@ -7,9 +7,21 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*.html")
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from %v", "Gin")
+		c.HTML(http.StatusOK, "index.html", nil)
+		// c.String(http.StatusOK, "Hello from %v", "Gin")
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	admin := r.Group("/admin")
+
+	admin.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin-overview.html", nil)
 	})
 
 	r.Run(":3000")
