4db50b2969510f590cc287b0ae18c94e52a98384
Miles <milesasp@gmail.com>
2019-12-14 13:24:13 +0800

    override from answer

--- src/app/controller.go
+++ src/app/controller.go
@@ -9,40 +9,50 @@ import (
 func registerRoutes() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/**/*.html")
-
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
-		// c.String(http.StatusOK, "Hello from %v", "Gin")
 	})
 
 	r.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", nil)
 	})
 
-	r.GET("/employees/:id/vacation", func(c *gin.Context){
+	r.GET("/employees/:id/vacation", func(c *gin.Context) {
 		id := c.Param("id")
 		timesOff, ok := TimesOff[id]
 
-		if !ok{
+		if !ok {
 			c.String(http.StatusNotFound, "404 - Page Not Found")
 			return
 		}
 
-		c.HTML(http.StatusOK, "vacation-voewview.html",
+		c.HTML(http.StatusOK, "vacation-overview.html",
 			map[string]interface{}{
 				"TimesOff": timesOff,
 			})
-
 	})
 
 	admin := r.Group("/admin")
-
 	admin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "admin-overview.html", nil)
 	})
 
+	admin.GET("/employees/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		employee, ok := employees[id]
+
+		if !ok {
+			c.String(http.StatusNotFound, "404 - Not Found")
+			return
+		}
+
+		c.HTML(http.StatusOK, "admin-employee-edit.html",
+			map[string]interface{}{
+				"Employee": employee,
+			})
+	})
+
 	r.Static("/public", "./public")
-	// r.StaticFS("/public", http.Dir("./public"))
 
 	return r
 }
